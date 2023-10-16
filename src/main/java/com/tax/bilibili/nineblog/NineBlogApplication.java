5ea6f50f4fd48d6b43b91a33de1996d3242a1688
sheip9 <sheip9@outlook.com>
2023-10-16 22:46:20 +0800

    init: first commit
    今天开始画饼，看我几天能搓完

---
+++ src/main/java/com/tax/bilibili/nineblog/NineBlogApplication.java
@@ -0,0 +1,13 @@
+package com.tax.bilibili.nineblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NineBlogApplication.class, args);
+    }
+
+}
