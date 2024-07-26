d8ccf73506c08c62aa132a9c8da704edbf37ea6e
sheip9 <sheip9@outlook.com>
2024-07-26 18:55:29 +0800

    update sql

--- server/src/main/java/tax/bilibili/nineblog/application/repository/ArticleRepository.kt
+++ server/src/main/java/tax/bilibili/nineblog/application/repository/ArticleRepository.kt
@@ -4,7 +4,6 @@ import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 import tax.bilibili.nineblog.application.entity.Article
 
 @Repository
@@ -14,8 +13,11 @@ interface ArticleRepository : R2dbcRepository<Article, Number> {
             " FROM Article") // 唉，要是能自动识别实体的表名就好了
     override fun findAll() : Flux<Article>
 
-    @Query("SELECT a.id, a.author_id, a.title, LEFT(a.content, 5) AS content, a.created_at, a.updated_at, a.visibility_level" +
-            " FROM Article ac " +
-            "LIMIT :limit")
+    @Query("SELECT id, author_id, title, LEFT(content, 5) AS content, created_at, updated_at, visibility_level" +
+            " FROM Article " +
+            " ORDER BY id DESC " +
+            " OFFSET :offset  " +
+            " LIMIT :limit "
+            )
     fun findAll(limit: Number = 10, offset: Number = 0) : Flux<Article>
 }
