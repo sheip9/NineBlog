dc8f7bbb727b0349d500759151f6930ad3305481
sheip9 <sheip9@outlook.com>
2024-07-28 10:25:17 +0800

    Main

--- server/src/main/java/tax/bilibili/nineblog/NineBlogApplication.kt
+++ server/src/main/java/tax/bilibili/nineblog/NineBlogApplication.kt
@@ -4,19 +4,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.ComponentScan
 
 @EnableCaching
 @ConfigurationPropertiesScan
 @SpringBootApplication
-@ComponentScan(basePackages = ["tax.bilibili.nineblog.application"])
+@ComponentScan(basePackages = ["tax.bilibili.nineblog.application", "tax.bilibili.nineblog.view"])
 open class NineBlogApplication
 
-@SpringBootApplication
-@ComponentScan(basePackages = ["tax.bilibili.nineblog.initialize"])
-open class NineBlogInitializeApplication
+//@SpringBootApplication
+//@ComponentScan(basePackages = ["tax.bilibili.nineblog.initialize"])
+//open class NineBlogInitializeApplication
+
+object AA {
+    lateinit var s : ConfigurableApplicationContext
+}
 
 fun main(args: Array<String>) {
-    runApplication<NineBlogApplication>(*args)
+    AA.s = runApplication<NineBlogApplication>(*args)
 }
 
