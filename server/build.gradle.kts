caa1b2ef4fe4dcdbced629d2d687feea84c3cd7d
sheip9 <sheip9@outlook.com>
2024-07-30 19:34:29 +0800

    build.gradle.kts: add package

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -23,13 +23,16 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 //    implementation("cn.dev33:sa-token-spring-boot3-starter:1.37.0")
+    implementation ("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor ("org.mapstruct:mapstruct-processor:1.5.5.Final")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation(kotlin("stdlib-jdk8"))
     testImplementation("io.projectreactor:reactor-test")
     runtimeOnly("io.asyncer:r2dbc-mysql:1.1.0")
     runtimeOnly("org.postgresql:r2dbc-postgresql:1.0.5.RELEASE")
