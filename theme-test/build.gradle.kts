b759234088c8ce21c7a51abe4ab3ca8ff7339b6e
sheip9 <sheip9@outlook.com>
2024-08-04 11:37:23 +0800

    漏提交了点东西

---
+++ theme-test/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+//    id("org.springframework.boot") version "3.2.4"
+//    id("io.spring.dependency-management") version "1.1.4"
+    alias(libs.plugins.kotlinJvm)
+    application
+}
+
+group = "tax.bilibili.nineblog"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.springframework:spring-context:6.1.11")
+//    compileOnly("org.springframework.boot:spring-boot-starter")
+    compileOnly(project(":server"))
+//    testImplementation("org.springframework.boot:spring-boot-starter-test")
+//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
