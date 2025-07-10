fad8f2deeb3386bd7eb3c330170f8d46963164a2
sheip9 <sheip9@outlook.com>
2025-07-10 16:51:47 +0800

    ExceptionAdvice.kt: validation failed message

--- server/src/main/kotlin/tax/bilibili/nineblog/application/advice/ExceptionAdvice.kt
+++ server/src/main/kotlin/tax/bilibili/nineblog/application/advice/ExceptionAdvice.kt
@@ -1,10 +1,14 @@
 package tax.bilibili.nineblog.application.advice
 
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.method.annotation.HandlerMethodValidationException
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
@@ -35,12 +39,19 @@ class ExceptionAdvice : ResponseEntityExceptionHandler() {
         return Mono.just(e)
     }
 
-//    override fun handleMethodArgumentNotValid(
-//        ex: MethodArgumentNotValidException,
-//        headers: HttpHeaders,
-//        status: HttpStatusCode,
-//        request: WebRequest
-//    ): ResponseEntity<Any> {
-//        return ResponseEntity("Validation failed: " + ex.bindingResult.toString(), HttpStatus.BAD_REQUEST)
-//    }
+    override fun handleHandlerMethodValidationException(
+        ex: HandlerMethodValidationException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        exchange: ServerWebExchange,
+    ): Mono<ResponseEntity<Any>> {
+//        ex.parameterValidationResults.first().resolvableErrors.first().defaultMessage
+        val res = ex.parameterValidationResults.joinToString(", ") {
+            it.resolvableErrors.joinToString(", ") { err ->
+                err.defaultMessage ?: ""
+            }
+        }
+        return Mono.just(ResponseEntity.status(status).body(res))
+    }
+
 }
