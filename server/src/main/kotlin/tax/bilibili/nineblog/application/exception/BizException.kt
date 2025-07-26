abdfe2e07ea148e724e2a53c82275fddab3924a2
sheip9 <sheip9@outlook.com>
2025-07-26 17:31:57 +0800

    BizException.kt: override fillInStackTrace to disable stacktrace

--- server/src/main/kotlin/tax/bilibili/nineblog/application/exception/BizException.kt
+++ server/src/main/kotlin/tax/bilibili/nineblog/application/exception/BizException.kt
@@ -3,4 +3,6 @@ package tax.bilibili.nineblog.application.exception
 abstract class BizException : RuntimeException {
     constructor() : super()
     constructor(message: String) : super(message)
+
+    override fun fillInStackTrace(): Throwable? = this
 }
